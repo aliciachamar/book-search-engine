240db14a20c5ea9e3d45741b1b305213074902e0
aliciachamar <aliciachamar@gmail.com>
2021-10-10 15:45:33 -0600

    added apollo server

--- server/server.js
+++ server/server.js
@@ -7,6 +7,9 @@ const { ApolloServer } = require('apollo-server-express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const server = new ApolloServer();
+server.applyMiddleware({ app });
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
